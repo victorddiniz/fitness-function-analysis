f6e147f51bb93de987cb6695ec2b076d59c1a24a
victorddiniz <victorddiniz@gmail.com>
2018-02-25 20:24:31 -0300

    feature(entrypoint): upload datasets and entrypoint

--- test.go
+++ test.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"strings"
 	"fmt"
 	"time"
 	"github.com/victorddiniz/fitness-function-analysis/experiments"
 )
 
 func main() {
-	experiment := experiments.NewExperiment(time.Now().UnixNano(), 10, 1, 1, 100000, "datasets/sun.txt")
+	experiment := experiments.NewExperiment(time.Now().UnixNano(), 10, 1, 1, 100000, "datasets/dj.txt")
 	results := experiment.Run()
 	
-	fmt.Println(results)
+	for _, fitValues := range results {
+		stringResults := make([]string, len(fitValues))
+		for i, value := range fitValues {
+			stringResults[i] = fmt.Sprintf("%f", value)
+		}
+		r := strings.Join(stringResults, ",")
+		fmt.Println(r)
+	}
 }
